afed161ab1c248b1f25e12235721a0198c1b9af2
Louis Camus <louis.camus@ekimetrics.com>
2020-11-16 23:52:51 +0100

    fonctionne

--- src/App.js
+++ src/App.js
@@ -22,39 +22,61 @@ function Email({email}) {
 function NameForm({  }) { //un nameform est une zone ou on peut ajouter un nom. reste a virer le handle submit...
   const [value, setValue] = React.useState("nameee");
   return ( // gérer le handlesubmit ci dessous
-    <div className="name">
+    <textarea>
       {value}
-    </div>
+    </textarea>
   );
 }
 
 function EmailForm({  }) { //un nameform est une zone ou on peut ajouter un nom. reste a virer le handle submit...
   const [value, setValue] = React.useState("email");
   return ( // gérer le handlesubmit ci dessous
-    <div className="email">
+    <textarea>
       {value}
-    </div>
+    </textarea>
   );
 }
 
-function SaveLineForm({addName,addEmail,index,saveLine}){
+function SaveLineForm({addLine,index}){
   
+  const [NewNames, setnameValue] = React.useState("");
+  const [NewEmails, setemailValue] = React.useState("");
+
+  const handleSubmit = () => {
+    if ((!NewNames) & (!NewEmails)) return;
+    addLine(NewNames,NewEmails,index);
+    setnameValue("");
+    setemailValue("");
+  }
+
+  const nothing = ()=>{
+    NewNames=NewNames;
+  }
+
   return(
-  <div className ="line">
-    <div className="name">
-      <NameForm
-        key={index}
-        index={index}
+  <div className ="Formline">
+    <div className="nameForm">
+      <form onSubmit={nothing}>
+        <input
+          type="text"
+          className="input"
+          NewNames={NewNames}
+          onChange={e => setnameValue(e.target.value)} // c'est ici qu'on affecte la valeur a setValue !
         />
+      </form>
     </div>
-    <div className="email">
-      <EmailForm
-        key={index}
-        index={index}
-     />
+    <div className="emailForm">
+      <form onSubmit={nothing}>
+        <input
+          type="text"
+          className="input"
+          NewEmails={NewEmails}
+          onChange={d => setemailValue(d.target.value)} // c'est ici qu'on affecte la valeur a setValue !
+        />
+      </form>
     </div>
     <div>
-      <button onClick={() => saveLine(addName,addEmail,index)}>SaveLine</button>
+      <button onClick={handleSubmit}>SaveLine</button>
     </div>
   </div>
   );
@@ -63,38 +85,22 @@ function SaveLineForm({addName,addEmail,index,saveLine}){
 
 function App() {
 
-  const [names, setNames] = React.useState([
-    {
-      text: "Donaldyy"
-    }
-  ]);
-  const [emails, setEmails] = React.useState([
-    {
-      text: "Donaldmail"
-    }
-  ]);
+  const [names, setNames] = React.useState([{text: "Donaldyy",text:"name"}]);
+  const [emails, setEmails] = React.useState([{text: "Donaldmail",text:"email"}]);
 
   const addName = text => {
     const newNames = [...names, { text }];
-    // setNames(newNames);  //ici le setNames représente la fonction d'appel
+    setNames(newNames);  //ici le setNames représente la fonction d'appel
   };
 
   const addEmail = text => {
     const newEmails = [...emails, { text }];
-    // setEmails(newEmails);
+    setEmails(newEmails);
   };
 
-//   const ActuNames = newNames => {
-//     setNames(newNames);
-//   };
-
-//   const ActuEmails = newEmails => {
-//     setEmails(newEmails);
-// };
-
-  const saveLine = (newNames, newEmails) => {
-    setNames(newNames);
-    setEmails(newEmails);
+  const addLine = (newNames, newEmails) => {
+    addName(newNames);
+    addEmail(newEmails);
   }
 
   return (
@@ -102,25 +108,29 @@ function App() {
     <h1>App</h1> 
       <div className="table">
       <h1>table</h1> 
-        <div className="Line"> 
-          <div className="name">
-            {names.map((name, index) => ( //il y a un map: c'est un affichage multiple du vecteur Namex selon la liste des "name"
+        <div className="line-list"> 
+          <div className="name-container">
+            <div className="name">
+              {names.map((name, index) => ( //il y a un map: c'est un affichage multiple du vecteur Namex selon la liste des "name"
               <Name
-              key={index}
-              index={index}
-              name={name}
-              />
-            ))}
-          </div>      
-          <div className="email">
-            {emails.map((email, index) => ( //il y a un map: c'est un affichage multiple
+                key={index}
+                index={index}
+                name={name}
+                />
+              ))}
+            </div>   
+          </div>   
+          <div className="email-container">
+            <div className="email">
+              {emails.map((email, index) => ( //il y a un map: c'est un affichage multiple
               <Email
-              key={index}
-              index={index}
-              email={email}
-              />
-            ))}
-          </div>     
+                key={index}
+                index={index}
+                email={email}
+                />
+              ))}
+            </div>     
+          </div>
         </div>
       </div>
       <div className="ajout">
@@ -129,9 +139,9 @@ function App() {
         <EmailForm addEmail={addEmail} /> */}
         {/* SaveLine est le format d'entrée. A coté on identifie la variable addName = fonction addName (le formulaire) */}
         <SaveLineForm 
-          addName={addName} 
-          addEmail={addEmail}
-          saveLine={saveLine}
+          // addName={addName} 
+          // addEmail={addEmail}
+          addLine={addLine}
           // index={index}
           />
         ],
--- src/Appsave2.js
+++ src/Appsave2.js
@@ -1,152 +1,152 @@
-import React from "react";
-import "./App.css";
+// import React from "react";
+// import "./App.css";
 
-function Name({name}) {
-  return (
-    <div className="name" >
-      {name.text}  
-    </div> 
-  );
-}
+// function Name({name}) {
+//   return (
+//     <div className="name" >
+//       {name.text}  
+//     </div> 
+//   );
+// }
 
-function Email({email}) {
-  return (
-    <div className="email" >
-      {email.text}  
-    </div> 
-  );
-}
-
-
-function saveLineForm({addName,addEmail,index}) {
-  return (
-    <div>
-      <button onClick={() => saveLine(newNames, newEmails) }>Save</button>
-    </div>
-  );
-}
+// function Email({email}) {
+//   return (
+//     <div className="email" >
+//       {email.text}  
+//     </div> 
+//   );
+// }
 
 
-function NameForm({ addName }) { //un nameform est une zone ou on peut ajouter un nom. reste a virer le handle submit...
-  const [value, setValue] = React.useState("nameee");
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    addName(value);
-    setValue("name2");
-  };
-  return ( // gérer le handlesubmit ci dessous
-    <form onSubmit={handleSubmit}> 
-      <input
-        type="text"
-        className="nameInput"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-    </form>
-  );
-}
-
-function EmailForm({ addEmail }) {
-  const [value, setValue] = React.useState("Email");
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    addEmail(value);
-    setValue("email2");
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="emailInput"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-    </form>
-  );
-}
+// function saveLineForm({addName,addEmail,index}) {
+//   return (
+//     <div>
+//       <button onClick={() => saveLine(newNames, newEmails) }>Save</button>
+//     </div>
+//   );
+// }
 
 
-function App() {
+// function NameForm({ addName }) { //un nameform est une zone ou on peut ajouter un nom. reste a virer le handle submit...
+//   const [value, setValue] = React.useState("nameee");
 
-  const [names, setNames] = React.useState([
-    {
-      text: "Donaldyy"
-    }
-  ]);
-  const [emails, setEmails] = React.useState([
-    {
-      text: "Donaldmail"
-    }
-  ]);
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//     if (!value) return;
+//     addName(value);
+//     setValue("name2");
+//   };
+//   return ( // gérer le handlesubmit ci dessous
+//     <form onSubmit={handleSubmit}> 
+//       <input
+//         type="text"
+//         className="nameInput"
+//         value={value}
+//         onChange={e => setValue(e.target.value)}
+//       />
+//     </form>
+//   );
+// }
 
-  const addName = text => {
-    const newNames = [...names, { text }];
-    // setNames(newNames);  //ici le setNames représente la fonction d'appel
-  };
+// function EmailForm({ addEmail }) {
+//   const [value, setValue] = React.useState("Email");
 
-  const addEmail = text => {
-    const newEmails = [...emails, { text }];
-    // setEmails(newEmails);
-  };
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//     if (!value) return;
+//     addEmail(value);
+//     setValue("email2");
+//   };
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input
+//         type="text"
+//         className="emailInput"
+//         value={value}
+//         onChange={e => setValue(e.target.value)}
+//       />
+//     </form>
+//   );
+// }
 
-//   const ActuNames = newNames => {
-//     setNames(newNames);
+
+// function App() {
+
+//   const [names, setNames] = React.useState([
+//     {
+//       text: "Donaldyy"
+//     }
+//   ]);
+//   const [emails, setEmails] = React.useState([
+//     {
+//       text: "Donaldmail"
+//     }
+//   ]);
+
+//   const addName = text => {
+//     const newNames = [...names, { text }];
+//     // setNames(newNames);  //ici le setNames représente la fonction d'appel
 //   };
 
-//   const ActuEmails = newEmails => {
+//   const addEmail = text => {
+//     const newEmails = [...emails, { text }];
+//     // setEmails(newEmails);
+//   };
+
+// //   const ActuNames = newNames => {
+// //     setNames(newNames);
+// //   };
+
+// //   const ActuEmails = newEmails => {
+// //     setEmails(newEmails);
+// // };
+
+//   const saveLine = (newNames, newEmails)=>{
+//     setNames(newNames);
 //     setEmails(newEmails);
-// };
+//   }
 
-  const saveLine = (newNames, newEmails)=>{
-    setNames(newNames);
-    setEmails(newEmails);
-  }
+//   return (
+//     <div className="app">
+//     <h1>App</h1> 
+//       <div className="table">
+//       <h1>table</h1> 
+//         <div className="Line"> 
+//           <div className="name">
+//             {names.map((name, index) => ( //il y a un map: c'est un affichage multiple du vecteur Namex selon la liste des "name"
+//               <Name
+//               key={index}
+//               index={index}
+//               name={name}
+//               />
+//             ))}
+//           </div>      
+//           <div className="email">
+//             {emails.map((email, index) => ( //il y a un map: c'est un affichage multiple
+//               <Email
+//               key={index}
+//               index={index}
+//               email={email}
+//               />
+//             ))}
+//           </div>     
+//         </div>
+//       </div>
+//       <div className="ajout">
+//         <h2>ajout</h2> 
+//         <NameForm addName={addName} /> 
+//         <EmailForm addEmail={addEmail} />
+//         {/* SaveLine est le format d'entrée. A coté on identifie la variable addName = fonction addName (le formulaire) */}
+//         <SaveLineForm 
+//           addName={addName} 
+//           addEmail={addEmail}
+//           saveLine={saveLine}
+//           />
+//         ],
+//       </div> 
 
-  return (
-    <div className="app">
-    <h1>App</h1> 
-      <div className="table">
-      <h1>table</h1> 
-        <div className="Line"> 
-          <div className="name">
-            {names.map((name, index) => ( //il y a un map: c'est un affichage multiple du vecteur Namex selon la liste des "name"
-              <Name
-              key={index}
-              index={index}
-              name={name}
-              />
-            ))}
-          </div>      
-          <div className="email">
-            {emails.map((email, index) => ( //il y a un map: c'est un affichage multiple
-              <Email
-              key={index}
-              index={index}
-              email={email}
-              />
-            ))}
-          </div>     
-        </div>
-      </div>
-      <div className="ajout">
-        <h2>ajout</h2> 
-        <NameForm addName={addName} /> 
-        <EmailForm addEmail={addEmail} />
-        {/* SaveLine est le format d'entrée. A coté on identifie la variable addName = fonction addName (le formulaire) */}
-        <SaveLineForm 
-          addName={addName} 
-          addEmail={addEmail}
-          saveLine={saveLine}
-          />
-        ],
-      </div> 
+//     </div>
+//   );
+// }
 
-    </div>
-  );
-}
-
-export default App;
+// export default App;
